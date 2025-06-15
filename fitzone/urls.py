b954d1368930d96519b51fe0e4598f38d92237d3
Danut Grigore <voicuion488@gmail.com>
2025-06-15 19:49:35 +0100

    Added smart navbar with links for login, register, dashboard, and logout

--- fitzone/urls.py
+++ fitzone/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views as home_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_views.home, name='home'),
     path('accounts/', include('allauth.urls')),
+    path('dashboard/', home_views.dashboard, name='dashboard'),
 ]
 
 
--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home/index.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'home/dashboard.html')
+
