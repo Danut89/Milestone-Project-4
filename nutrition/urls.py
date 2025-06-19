404cdd18e23a56d0c8615e7280e8e6686629a624
Danut Grigore <voicuion488@gmail.com>
2025-06-19 12:28:49 +0100

    Add dynamic recipe and supplement views and templates  - Created views to display recipes and supplements dynamically - Added routes for /recipes/ and /supplements/ in nutrition app - Designed Bootstrap-based templates for both pages - Verified content renders from admin-added entries

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('meal-plans/', views.meal_plans, name='meal_plans'),
-    path('recipes/', views.recipes, name='recipes'),
-    path('supplements/', views.supplements, name='supplements'),
+    path('recipes/', views.recipes_list, name='recipes'),
+    path('supplements/', views.supplements_list, name='supplements'),
 ]
 
--- nutrition/views.py
+++ nutrition/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from .models import MealPlan
+from .models import MealPlan, Recipe, Supplement
 
 def meal_plans(request):
     plans = MealPlan.objects.all()
     return render(request, 'nutrition/meal_plans.html', {'plans': plans})
 
+def recipes_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'nutrition/recipes.html', {'recipes': recipes})
 
-def recipes(request):
-    return render(request, 'nutrition/recipes.html')
-
-def supplements(request):
-    return render(request, 'nutrition/supplements.html')
+def supplements_list(request):
+    supplements = Supplement.objects.all()
+    return render(request, 'nutrition/supplements.html', {'supplements': supplements})
 
