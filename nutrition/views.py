f071109171cf5af7accf60d8f17dd62ff85b4b33
Danut Grigore <voicuion488@gmail.com>
2025-07-01 13:57:49 +0100

    Improved daily breakdown section vissualy and added pagination to meal plans page with improved UX - Implemented dynamic pagination in meal_plans.html - Fixed duplication of page numbers when on first/last page

--- nutrition/views.py
+++ nutrition/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import MealPlan, Recipe, Supplement
+from django.core.paginator import Paginator
 
 def nutrition_home(request):
     return render(request, 'nutrition/home.html')
 
 def meal_plans(request):
-    plans = MealPlan.objects.all()
-    return render(request, 'nutrition//meal_plans.html', {'plans': plans})
+    all_plans = MealPlan.objects.all()
+    paginator = Paginator(all_plans, 6)  # Display 6 per page
+    page_number = request.GET.get('page')
+    plans = paginator.get_page(page_number)
+    return render(request, 'nutrition/meal_plans.html', {'plans': plans})
 
 
 def meal_plan_detail(request, pk):
