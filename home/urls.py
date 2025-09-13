1013562119f70d68618d8f15a22dd3ef27303d77
Danut Grigore <voicuion488@gmail.com>
2025-09-13 13:03:52 +0100

    Removed files added just for database reinitialization

--- home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from home.views import run_migrations_view, collect_static_view
-
-urlpatterns = [
-    path('run-migrations/', run_migrations_view),
-    path('collect-static/', collect_static_view),
-]
